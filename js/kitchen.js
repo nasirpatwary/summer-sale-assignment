421d98ad3d92a23873577c5fb0375e5acfbfe511
nasirhpatwary <nasirhpatwary@gmail.com>
2023-08-21 19:40:23 +0600

    first commit

---
+++ js/kitchen.js
@@ -0,0 +1,18 @@
+function handelClickKAccessories() {
+    const accessoriesField = document.getElementById('k-accessories');
+    const accessoriesValueText = accessoriesField.innerText;
+    const accessories = accessoriesValueText;
+    console.log(accessories);
+}
+function handelClickKAccessories() {
+    const accessoriesField = document.getElementById('k-accessories');
+    const accessoriesValueText = accessoriesField.innerText;
+    const accessories = accessoriesValueText;
+    console.log(accessories);
+}
+function handelClickHomeCooker() {
+    const cookerField = document.getElementById('home-cooker');
+    const cookerValueText = cookerField.innerText;
+    const cooker = cookerValueText;
+    console.log(cooker);
+}
